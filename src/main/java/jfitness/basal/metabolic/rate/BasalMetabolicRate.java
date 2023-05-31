b48e40e515e423ae9a3365795ba6fb1237f715b6
Saul <saul.rodriguez.naranjo@gmail.com>
2023-05-31 20:49:45 +0200

    Added javadoc to BMRFormula and BasalMetabolicRate interfaces

--- src/main/java/jfitness/basal/metabolic/rate/BasalMetabolicRate.java
+++ src/main/java/jfitness/basal/metabolic/rate/BasalMetabolicRate.java
@@ -1,9 +1,46 @@
 package jfitness.basal.metabolic.rate;
 
 /**
- *
- * @author SaulWorkStation
- * @param <ActivityLevelT>
+ * This interface represents basal metabolic rate calculated with a formula
+ * that supports a certain activity level criteria.
+ * 
+ * <p>
+ * The basal metabolic rate is the rate of energy expenditure per unit time by
+ * endothermic animals at rest. This rate is often reported in units ranging from
+ * watt(joule/second) to joule per hour per kilogram of body mass (J/(h*kg)).
+ * </p>
+ * 
+ * <p><strong>
+ * In a nutritional context basal metabolic rate is usually expressed in kilocalories
+ * per day (Kcal/day).
+ * </strong></p>
+ * 
+ * <p>
+ * Metabolism comprises the processes that the body needs to keep functioning at rest.
+ * This includes processes like breathing, blood circulation, cell growth, brain
+ * and nerve function, contraction of muscles, etc.
+ * </p>
+ * 
+ * <p>
+ * Ultimately the basal metabolic rate affects the rate that an individual burns
+ * calories and whether said individual maintains, gains or loses weight. This
+ * rate accounts for 60 to 75% of the daily calorie expenditure, hence the need
+ * to accompany this value with an activity level that will provide the full
+ * individual's kilocalorie expenditure per day.
+ * </p>
+ * 
+ * <p>
+ * Basal metabolic rate is calculated by an equation, and multiplied by an activity
+ * level compatible with said equation. Please refer to 
+ * {@link jfitness.basal.metabolic.rate.formula.BMRFormula} for more information.
+ * </p>
+ * 
+ * @author Saúl Rodríguez Naranjo
+ * 
+ * @param <ActivityLevelT> The activity level compatible with the BMR formula that
+ *                         calculated this basal metabolic rate.
+ * 
+ * @see jfitness.basal.metabolic.rate.formula.BMRFormula
  */
 public interface BasalMetabolicRate<ActivityLevelT extends Enum<ActivityLevelT>> {
     
--- src/main/java/jfitness/basal/metabolic/rate/formula/BMRFormula.java
+++ src/main/java/jfitness/basal/metabolic/rate/formula/BMRFormula.java
@@ -1,9 +1,32 @@
 package jfitness.basal.metabolic.rate.formula;
 
 /**
- *
+ * This interface represents an equation that calculates an individual's basal
+ * metabolic rate (BMR).
+ * 
+ * <p>
+ * <strong>
+ * For more information about basal metabolic rest, check 
+ * {@link jfitness.basal.metabolic.rate.BasalMetabolicRate}.
+ * </strong>
+ * </p>
+ * 
+ * <p>
+ * Basal metabolic rate is influenced by several factors. This includes body weight,
+ * lean body mass, height, age, the person's sex, etc. Formulas estimate an
+ * individual's basal metabolic rate using these paremeters, though depending on
+ * the formula, certain parameters are used or not.
+ * </p>
+ * 
+ * <p>
+ * Once basal metabolic rate is calculated, normally an activity level is used to
+ * provide the individual's daily calorie expenditure. <strong>This activity level must
+ * be compatible with this formula.</strong>
+ * </p>
+ * 
  * @author Saúl Rodríguez Naranjo
- * @param <ActivityLevelT>
+ * 
+ * @param <ActivityLevelT> The activity level that this formula is compatible with.
  */
 public interface BMRFormula<ActivityLevelT extends Enum<ActivityLevelT>> {
     
