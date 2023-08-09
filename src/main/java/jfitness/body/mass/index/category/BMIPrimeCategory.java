d64e888cfa82d1fd8ac78d5b453ab479af4502ab
Saul <saul.rodriguez.naranjo@gmail.com>
2023-08-09 22:51:32 +0200

    BMI Prime categories enum

--- src/main/java/jfitness/body/mass/index/category/BMIPrimeCategory.java
+++ src/main/java/jfitness/body/mass/index/category/BMIPrimeCategory.java
@@ -8,35 +8,50 @@ package jfitness.body.mass.index.category;
 public enum BMIPrimeCategory
 {
     /**
-     *
+     * This category corresponds to a body mass index value lesser than 0.64
+     * kg/m^2 (kilogram/square meter)
      */
     UNDERWEIGHT_SEVERE_THINNESS("Underweight (severe thinness)"),
+    
     /**
-     *
+     * This category corresponds to a body mass index value between 0.64
+     * (included) and 0.67 (included) kg/m^2 (kilogram/square meter)
      */
     UNDERWEIGHT_MODERATE_THINNESS("Underweight (moderate thinness)"),
+    
     /**
-     *
+     * This category corresponds to a body mass index value between 0.68
+     * (included) and 0.73 (included) kg/m^2 (kilogram/square meter)
      */
     UNDERWEIGHT_MILD_THINNESS("Underweight (mild thinness)"),
+    
     /**
-     *
+     * This category corresponds to a body mass index value between 0.74
+     * (included) and 0.99 (included) kg/m^2 (kilogram/square meter)
      */
     NORMAL_RANGE("Normal range"),
+    
     /**
-     *
+     * This category corresponds to a body mass index value between 1
+     * (included) and 1.19 (included) kg/m^2 (kilogram/square meter)
      */
     OVERWEIGHT_PRE_OBESE("Overweight (pre-obese)"),
+    
     /**
-     *
+     * This category corresponds to a body mass index value between 1.2
+     * (included) and 1.39 (included) kg/m^2 (kilogram/square meter)
      */
     OBESE_CLASS_1("Obese (class I)"),
+    
     /**
-     *
+     * This category corresponds to a body mass index value between 1.4
+     * (included) and 1.59 (included) kg/m^2 (kilogram/square meter)
      */
     OBESE_CLASS_2("Obese (class II)"),
+    
     /**
-     *
+     * This category corresponds to a body mass index value equal or greater
+     * than 1.6 kg/m^2 (kilogram/square meter)
      */
     OBESE_CLASS_3("Obese (class III)");
 
